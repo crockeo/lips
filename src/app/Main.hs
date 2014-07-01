2b94f4ba2287d62ecbb83a296a2fce444900877a
Cerek Hillen <crockeo@gmail.com>
2014-06-30 23:24:47 -0400

    Added a launcher for the language.

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+--------------------
+-- Global Imports --
+import Language.Lips.Static
+import Language.Lips.REPL
+
+import System.Environment
+
+----------
+-- Code --
 main :: IO ()
-main = undefined
+main = do
+  args <- getArgs
+
+  case args of
+    []        -> startRepl
+    (x:[])    -> runStatic x
+    otherwise -> putStrLn "Proper usage: lips [script filepath]"
