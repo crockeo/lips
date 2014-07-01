c5b2e9dd1ae4345609e42a24dc5936afa89d3685
Cerek Hillen <crockeo@gmail.com>
2014-06-30 22:34:13 -0400

    Added the parser, and decided to go with Parsec instead of Attoparsec.

---
+++ src/lib/Language/Lips/Parser.hs
@@ -0,0 +1,87 @@
+module Language.Lips.Parser where
+
+--------------------
+-- Global Imports --
+import Text.Parsec.String
+import Text.Parsec hiding (spaces)
+
+import Control.Monad
+
+import Data.Monoid
+
+-------------------
+-- Local Imports --
+import Language.Lips.LanguageDef
+
+----------
+-- Code --
+
+-- Matching a symbol
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+-- One or more spaces
+spaces :: Parser ()
+spaces = skipMany1 space
+
+-- Parsing an LAtom
+parseLAtom :: Parser LipsVal
+parseLAtom = do
+  first <-        choice [letter, symbol]
+  rest  <- many $ choice [letter, symbol, digit]
+
+  return $ case first : rest of
+    "#t"  -> LBool True
+    "#f"  -> LBool False
+    other -> LAtom other
+
+-- Parsing an LList
+parseLList :: Parser LipsVal
+parseLList = liftM LList $ between (char '(') (char ')') $ sepBy lipsParser spaces
+
+-- Parsing an LDottedList
+parseLDottedList :: Parser LipsVal
+parseLDottedList =
+  between (char '(') (char ')') $ do
+    list <- lipsParser `endBy` spaces
+    end  <- char '.' >> spaces >> lipsParser
+
+    return $ LDottedList list end
+
+-- Parsing an LNumber
+parseLNumber :: Parser LipsVal
+parseLNumber = do
+  whole <- many1 digit
+  decim <- option "0" $ (char '.' >> many1 digit)
+
+  return $ LNumber $ (read $ mconcat [whole, ".", decim] :: Double)
+
+-- Parsing an LString
+parseLString :: Parser LipsVal
+parseLString = liftM LString $ between (char '"') (char '"') (many1 $ noneOf "\"")
+
+-- Single-quote syntax
+parseQuoted :: Parser LipsVal
+parseQuoted = do
+  char '\''
+  lips <- lipsParser
+  return $ LList [LAtom "quote", lips]
+
+-- A singular lips parser that applies 
+lipsParser :: Parser LipsVal
+lipsParser =
+  choice $ map try parsers
+  where parsers = [ parseLAtom
+                  , parseLList
+                  , parseLDottedList
+                  , parseLNumber
+                  , parseLString
+                  , parseQuoted
+                  ]
+
+-- Parsing out a lips value
+parseLips :: String -> LipsVal
+parseLips input =
+  case parse lipsParser "parseLips" input of
+    Left  err -> error $ show err
+    Right val -> val
