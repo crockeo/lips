f6350ab27d366feeaf29e6b0b99086eeb97f6a9d
Cerek Hillen <crockeo@gmail.com>
2014-06-30 22:53:47 -0400

    Implemented a small REPL.

---
+++ src/lib/Language/Lips/REPL.hs
@@ -0,0 +1,41 @@
+module Language.Lips.REPL where
+
+--------------------
+-- Global Imports --
+import System.IO
+
+-------------------
+-- Local Imports --
+import Language.Lips.Evaluator
+import Language.Lips.Parser
+
+----------
+-- Code --
+
+-- Printing the REPL header
+printHeader :: IO ()
+printHeader = do
+  putStrLn "--------------------"
+  putStrLn "      lips REPL"
+  putStrLn "Enter 'quit' to exit"
+  putStrLn ""
+  hFlush stdout
+
+-- The REPL itself
+repl :: IO ()
+repl = do
+  putStr "lips} "
+  hFlush stdout
+
+  line <- getLine
+
+  case line of
+    "quit" -> return ()
+    other  -> do
+      putStrLn $ show $ eval $ parseLips other
+      hFlush stdout
+      repl
+
+-- Starting the REPL
+startRepl :: IO ()
+startRepl = printHeader >> repl
