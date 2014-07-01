3b74e717d17f0505f563ce46cf3c8b5744703904
Cerek Hillen <crockeo@gmail.com>
2014-06-30 21:29:34 -0400

    Added a basic language data structure definition.

---
+++ src/lib/Language/Lips/Language.hs
@@ -0,0 +1,19 @@
+module Language.Lips.Language where
+
+import Data.Monoid
+
+data LipsVal = LAtom       String
+             | LList       [LipsVal]
+             | LDottedList [LipsVal] LipsVal
+             | LNumber     Double
+             | LString     String
+             | LBool       Bool
+
+instance Show LipsVal where
+  show (LAtom       name  ) = name
+  show (LList       list  ) = mconcat ["(", foldl1 (\a b -> a ++ " " ++ b) $ map (show) list, ")"]
+  show (LDottedList list v) = mconcat ["(", show $ LList list, " . ", show v, ")"]
+  show (LNumber     number) = show number
+  show (LString     string) = show string
+  show (LBool       True  ) = "#t"
+  show (LBool       False ) = "#f"
