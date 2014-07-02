a1f8c09765b89ae224fac38e0b1110bf4c08f8a4
Cerek Hillen <crockeo@gmail.com>
2014-07-01 21:17:16 -0400

    Added function application.

--- src/lib/Language/Lips/Evaluator.hs
+++ src/lib/Language/Lips/Evaluator.hs
@@ -3,11 +3,14 @@ module Language.Lips.Evaluator where
 -------------------
 -- Local Imports --
 import Language.Lips.LanguageDef
+import Language.Lips.Base
 
 ----------
 -- Code --
 
 -- Evaluating a LipsVal operation
 eval :: LipsVal -> LipsVal
+eval (LAtom func                ) = getPrimitive func []
 eval (LList [LAtom "quote", val]) = val
+eval (LList (LAtom func:args)   ) = getPrimitive func $ map eval args
 eval other                        = other
