59cae6db1822cd5347e341f8432f606370438030
Cerek Hillen <crockeo@gmail.com>
2014-07-03 12:24:59 -0400

    Made REPL and Static both use the new stateful eval.

--- src/lib/Language/Lips/REPL.hs
+++ src/lib/Language/Lips/REPL.hs
@@ -2,12 +2,17 @@ module Language.Lips.REPL where
 
 --------------------
 -- Global Imports --
+import qualified Data.Map.Strict as Map
+
+import Control.Monad.State
+
 import System.IO
 
 -------------------
 -- Local Imports --
 import Language.Lips.Evaluator
 import Language.Lips.Parser
+import Language.Lips.State
 
 ----------
 -- Code --
@@ -22,20 +27,22 @@ printHeader = do
   hFlush stdout
 
 -- The REPL itself
-repl :: IO ()
+repl :: ProgramState ()
 repl = do
-  putStr "lips} "
-  hFlush stdout
+  liftIO $ putStr "lips} "
+  liftIO $ hFlush stdout
 
-  line <- getLine
+  line <- liftIO $ getLine
 
   case line of
     "quit" -> return ()
     other  -> do
-      putStrLn $ show $ eval $ lips other
-      hFlush stdout
+      newEval $ lips other
+      liftIO $ hFlush stdout
       repl
 
 -- Starting the REPL
 startRepl :: IO ()
-startRepl = printHeader >> repl
+startRepl = do
+  printHeader
+  evalStateT repl $ Program { variables = Map.fromList [] }
--- src/lib/Language/Lips/Static.hs
+++ src/lib/Language/Lips/Static.hs
@@ -2,18 +2,33 @@ module Language.Lips.Static where
 
 --------------------
 -- Global Imports --
+import qualified Data.Map.Strict as Map
+
+import Control.Monad.State
+
 import System.IO
 
 -------------------
 -- Local Imports --
+import Language.Lips.LanguageDef
 import Language.Lips.Evaluator
 import Language.Lips.Parser
+import Language.Lips.State
 
 ----------
 -- Code --
+
+-- Running a list of LipsVals
+runList :: [LipsVal] -> ProgramState ()
+runList []     = return ()
+runList (x:xs) = do
+  newEval x
+  liftIO $ hFlush stdout
+
+  runList xs
 
 -- From a static file
 runStatic :: FilePath -> IO ()
 runStatic path = do
   contents <- readFile path
-  mapM_ (\lip -> (putStrLn $ show $ eval lip) >> hFlush stdout) $ manyLips contents
+  evalStateT (runList $ manyLips contents) $ Program { variables = Map.fromList [] }
