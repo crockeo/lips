d16ca23a4efc4af9c8b1c2078cb86426cc2c9127
Cerek Hillen <crockeo@gmail.com>
2014-07-04 02:20:12 -0400

    Added an error-handling wrapper.

---
+++ src/lib/Language/Lips/Error.hs
@@ -0,0 +1,21 @@
+module Language.Lips.Error where
+
+----------
+-- Code --
+
+-- Error types
+data ErrorType = VariableNotDefinedError
+  deriving (Eq, Show, Read)
+
+-- The actual error monad
+data Error a = Success a | Error ErrorType
+
+-- Showing an error
+instance (Show a) => Show (Error a) where
+  show (Success a) = "Success " ++ show a
+  show (Error   t) = "Error " ++ show t
+
+-- A functor instance
+instance Functor Error where
+  fmap fn (Success a) = Success $ fn a
+  fmap fn (Error   t) = Error t
