da09926dde0cbf4ef2625b489c09c92f6d4f5358
Crockeo <crockeo@gmail.com>
2014-07-08 19:32:55 -0400

    Added functions to getting and setting the values of strings and lists.

--- src/lib/Language/Lips/Base.hs
+++ src/lib/Language/Lips/Base.hs
@@ -24,10 +24,41 @@ makeBinaryOperator op =
         toNum (LNumber n) = Success n
         toNum other       = Error InvalidTypeError
 
+-- Getting an element from a list/string
+getRaw :: [a] -> Int -> Error a
+getRaw xs n
+  | n >= length xs = Error InvalidArrayIndexError
+  | n < 0          = Error InvalidArrayIndexError
+  | otherwise      = Success $ xs !! n
+
+get :: [LipsVal] -> Error LipsVal
+get (LList   xs:LNumber index:[]) =                            getRaw xs $ floor index
+get (LString xs:LNumber index:[]) = liftM (LString . return) $ getRaw xs $ floor index
+get xs                            = Error InvalidFunctionApplicationError
+
+-- Setting an element in a list/array
+setRaw :: [a] -> Int -> a -> Error [a]
+setRaw l n a
+  | n >= length l = Error InvalidArrayIndexError
+  | n < 0         = Error InvalidArrayIndexError
+  | otherwise     = Success $ setRaw' l n a 0
+  where setRaw' :: [a] -> Int -> a -> Int -> [a]
+        setRaw' []     _ _ _ = []
+        setRaw' (x:xs) n a c
+          | n == c    = a : setRaw' xs n a (c + 1)
+          | otherwise = x : setRaw' xs n a (c + 1)
+
+set :: [LipsVal] -> Error LipsVal
+set (LList   xs:LNumber index:x:[])              = liftM LList   $ setRaw xs (floor index) x
+set (LString xs:LNumber index:LString (x:[]):[]) = liftM LString $ setRaw xs (floor index) x
+set xs                                           = Error InvalidFunctionApplicationError
+
 -- The base primitives
 basePrimitives :: Map.Map String Primitive
 basePrimitives = Map.fromList [ ("+", makeBinaryOperator (+))
                               , ("-", makeBinaryOperator (-))
                               , ("*", makeBinaryOperator (*))
                               , ("/", makeBinaryOperator (/))
+                              , ("get", get)
+                              , ("set", set)
                               ]
